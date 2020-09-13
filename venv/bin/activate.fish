1f032979996761d333018f9e8332c2a51d98981d
Kevin Lee <kevin5lee@outlook.com>
2020-09-12 17:17:05 -0700

    new venv

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/kevinlee/Desktop/supy/venv"
+set -gx VIRTUAL_ENV "/Users/kevinlee/Desktop/Supy/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
